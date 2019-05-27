d91c66270de18749f278a2998c518c294a44afb2
TakeMyHand <prey4@TakeMyHands-MacBook-Air.local>
2019-05-27 23:28:05 +0530

    subdomain script modified

--- wreckon.sh
+++ wreckon.sh
@@ -16,15 +16,20 @@ case $method in
 "sdbf")
 touch $result_file
 echo "brute-forcing $domain subdomains now"
+python ~/Pentesting/knock/knockpy/knockpy.py -w ~/Pentesting/knock/knockpy/wordlist/test.txt -c $domain
+cat *.csv | cut -d "," -f 4
+rm *.csv
 ;;
 "sdd")
 touch $result_file
 echo "using aquatone-discover on $domain now"
 aquatone-discover --domain $domain
-echo ~/aquatone/$domain/hosts.txt | cut -d "," -f 1
+cat ~/aquatone/$domain/hosts.txt | cut -d "," -f 1
 ;;
 *)
 echo "please enter either sdbf or sdd as argument"
 ;;
 esac
 
+
+#todo- add virustotal API key in knock config.json
