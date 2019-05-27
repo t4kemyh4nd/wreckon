61f36cea8ff526d0e11c61c03dba7d8e14741021
TakeMyHand <prey4@TakeMyHands-MacBook-Air.local>
2019-05-28 00:28:09 +0530

    fixed output

--- wreckon.sh
+++ wreckon.sh
@@ -3,7 +3,7 @@
 
 if [ $# -eq 0 ] || [ $# -eq 1 ];
  then
-    echo "Usage: ./wreckon.sh sdbf/sdd domain"
+    echo "Usage: ./wreckon.sh sdbf/sdd/dbf domain"
     exit 1
 fi
 
@@ -16,18 +16,18 @@ case $method in
 "sdbf")
 touch $result_file
 echo "brute-forcing $domain subdomains now"
-python ~/Pentesting/knock/knockpy/knockpy.py -w ~/Pentesting/knock/knockpy/wordlist/test.txt -c $domain
+python ~/Pentesting/knock/knockpy/knockpy.py -w ~/Pentesting/knock/knockpy/wordlist/test.txt -c $domain > /dev/null
 cat *.csv | cut -d "," -f 4
 rm *.csv
 ;;
 "sdd")
 touch $result_file
 echo "using aquatone-discover on $domain now"
-aquatone-discover --domain $domain
+aquatone-discover --domain $domain > /dev/null
 cat ~/aquatone/$domain/hosts.txt | cut -d "," -f 1
 ;;
 "dbf")
-python3 ~/Pentesting/dirsearch/dirsearch.py -b -u $domain -e * --plain-text-report=result.txt
+python3 ~/Pentesting/dirsearch/dirsearch.py -b -u $domain -w ameya.txt -e * --plain-text-report=result.txt > /dev/null
 cat result.txt
 rm result.txt
 ;;
