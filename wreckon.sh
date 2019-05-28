13729733c57226b735600e53719045b0ffd1f7bb
TakeMyHand <prey4@TakeMyHands-MacBook-Air.local>
2019-05-28 14:27:32 +0530

    added nikto

--- wreckon.sh
+++ wreckon.sh
@@ -12,35 +12,51 @@ result_file="/tmp/result.txt"
 method=$1
 domain=$2
 
-dirsearch () {
+dirsearch_scan () {
 	python3 ~/Pentesting/dirsearch/dirsearch.py -b -u $1 -w test.txt -e * --plain-text-report=result.txt > /dev/null
 	cat result.txt
 	rm result.txt
 	exit 0
 }
 
-knock () {
+knock_scan () {
 	echo "brute-forcing $1 subdomains now"
 	python ~/Pentesting/knock/knockpy/knockpy.py -w ~/Pentesting/knock/knockpy/wordlist/wordlist.txt -c $1 > /dev/null
 	cat *.csv | cut -d "," -f 4
 	rm *.csv
 }
 
-aquatone() {
+aquatone_scan () {
 	echo "using aquatone-discover on $1 now"
 	aquatone-discover --domain $1 > /dev/null
 	cat ~/aquatone/$1/hosts.txt | cut -d "," -f 1
 }
 
+nikto_scan () {
+	nikto -host $1 -output result.txt > /dev/null
+	cat result.txt
+}
+
+niktossl_scan () {
+	nikto -host $1 -ssl -output result.txt > /dev/null
+	cat result.txt
+}
+
 case $method in
 "sdbf")
-knock $domain
+knock_scan $domain
 ;;
 "sdd")
-aquatone $domain
+aquatone_scan $domain
 ;;
 "dbf")
-dirsearch $domain
+dirsearch_scan $domain
+;;
+"nikto")
+nikto_scan $domain
+;;
+"niktossl")
+niktossl_scan $domain
 ;;
 *)
 echo "please enter either sdbf or sdd as argument"
